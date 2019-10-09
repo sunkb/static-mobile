845ef9a4abb6e82299674c1ce0b611725288f205
chenyuwangzi <15558177820@163.com>
2019-10-09 03:02:16 -0500

    打包

--- pages/teacher/good_teacher_recommendation/index.vue
+++ pages/teacher/good_teacher_recommendation/index.vue
@@ -326,40 +326,40 @@ export default {
           this.teacherMsg.info.video = videoObj;
         }
         //上课视频
-        if (res.info && res.info.recommendation) {
-          let videoArr = [];
-          videoArr = res.info.recommendation.videos;
-          if (videoArr && videoArr.length > 0) {
-            videoArr.map((item, index) => {
-              let videoObj = {
-                autoplay: false, //如果true,浏览器准备好时开始回放。
-                muted: false, // 默认情况下将会消除任何音频。
-                loop: false, // 导致视频一结束就重新开始。
-                preload: "auto", // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
-                language: "zh-CN",
-                aspectRatio: "16:9", // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
-                fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
-                sources: [
-                  {
-                    type: "video/mp4", //这里的种类支持很多种：基本视频格式、直播、流媒体等，具体可以参看git网址项目
-                    src: "" //url地址
-                  }
-                ],
-                poster:
-                  "https://qn-static.landi.com/uploadtool56510002dc36f24b334a80a295fe3efc.png", //你的封面地址
-                notSupportedMessage: "此视频暂无法播放，请稍后再试", //允许覆盖Video.js无法播放媒体源时显示的默认信息。
-                controlBar: {
-                  timeDivider: true,
-                  durationDisplay: true,
-                  remainingTimeDisplay: false,
-                  fullscreenToggle: true //全屏按钮
-                }
-              };
-              videoObj.sources[0].src = item;
-              this.videoList.push(videoObj);
-            });
-          }
-        }
+        // if (res.info && res.info.recommendation) {
+        //   let videoArr = [];
+        //   videoArr = res.info.recommendation.videos;
+        //   if (videoArr && videoArr.length > 0) {
+        //     videoArr.map((item, index) => {
+        //       let videoObj = {
+        //         autoplay: false, //如果true,浏览器准备好时开始回放。
+        //         muted: false, // 默认情况下将会消除任何音频。
+        //         loop: false, // 导致视频一结束就重新开始。
+        //         preload: "auto", // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
+        //         language: "zh-CN",
+        //         aspectRatio: "16:9", // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
+        //         fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
+        //         sources: [
+        //           {
+        //             type: "video/mp4", //这里的种类支持很多种：基本视频格式、直播、流媒体等，具体可以参看git网址项目
+        //             src: "" //url地址
+        //           }
+        //         ],
+        //         poster:
+        //           "https://qn-static.landi.com/uploadtool56510002dc36f24b334a80a295fe3efc.png", //你的封面地址
+        //         notSupportedMessage: "此视频暂无法播放，请稍后再试", //允许覆盖Video.js无法播放媒体源时显示的默认信息。
+        //         controlBar: {
+        //           timeDivider: true,
+        //           durationDisplay: true,
+        //           remainingTimeDisplay: false,
+        //           fullscreenToggle: true //全屏按钮
+        //         }
+        //       };
+        //       videoObj.sources[0].src = item;
+        //       this.videoList.push(videoObj);
+        //     });
+        //   }
+        // }
         if (infoData.info.weekdays) {
           this.weekdays = infoData.info.weekdays;
         }
@@ -400,11 +400,9 @@ export default {
     tabChange(index) {
       alert(1)
       this.tabIndex = index;
-      return false;
-      debugger;
-      //this.commentList = [];
-      //this.currentPage = 1;
-      //this.getTeacherScoreFn();
+      this.commentList = [];
+      this.currentPage = 1;
+      this.getTeacherScoreFn();
     },
     //获取老师标签
     async getTag() {
