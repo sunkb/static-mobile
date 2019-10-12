eb2caae7e976350f3552a134c87f6eae5b4c1cc3
Jimmy张靖于 <jimzjy@gmail.com>
2019-10-12 20:08:58 +0800

    1

--- pages/presentation/index.vue
+++ pages/presentation/index.vue
@@ -333,7 +333,7 @@ export default {
         -webkit-line-clamp: 2;
         -webkit-box-orient: vertical;  
         overflow: hidden;
-        max-height: 86px;
+        max-lines: 2;
       }
     }
   }
--- pages/presentation/signup/step2/index.vue
+++ pages/presentation/signup/step2/index.vue
@@ -226,7 +226,7 @@ $topic-item-width: 480px;
       -webkit-line-clamp: 2;
       -webkit-box-orient: vertical;  
       overflow: hidden;
-      max-height: 86px;
+      max-lines: 2;
     }
 
     &-selector {
