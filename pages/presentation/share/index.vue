0723a71b8d58e68b5171beea89fce039cec5e6f7
Jimmy张靖于 <jimzjy@gmail.com>
2019-10-12 16:29:02 +0800

    1

--- pages/presentation/share/index.vue
+++ pages/presentation/share/index.vue
@@ -94,7 +94,8 @@ export default {
     async initData() {
       const { activity_id, work_id } = this.$route.query
       // const url = window.location.href
-      const url = decodeURIComponent(location.href.split('?')[0])
+      // const url = decodeURIComponent(location.href.split('?')[0])
+      const url = `https://release6.landi.com/static-web/mobile/presentation/share`
       const res = await axios.get(`${API.WORK}?activity_id=${activity_id}&url=${url}&work_id=${work_id}`)
       if (!res.status) {
         this.$refs['toast'].showToast(res.info)
