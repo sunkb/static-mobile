e1678fe40eda42dd6571ac28b669f1f60506d4d2
Jimmy张靖于 <jimzjy@gmail.com>
2019-10-14 19:55:24 +0800

    1

--- pages/presentation/share/index.vue
+++ pages/presentation/share/index.vue
@@ -97,7 +97,20 @@ export default {
 
       if (res.status) {
         // await this.initData();
-        this.liked = !this.liked
+        // this.liked = !this.liked
+        const { activity_id, work_id } = this.$route.query
+        const res = await axios.get(`${API.WORK}?activity_id=${activity_id}&work_id=${work_id}`)
+        if (!res.status) {
+          this.$refs['toast'].showToast(res.info)
+          return
+        }
+        const work = res.data.work;
+        this.stuData = {
+          videoSrc: work.video_url,
+          like: work.zan,
+          name: work.en_name
+        }
+        this.liked = work.is_zan
       }else{
         this.$refs['toast'].showToast(res.info)
       }
