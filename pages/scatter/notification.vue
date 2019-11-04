b075bc596ad862329ef1091192fbcf2d82c1ba04
jiafan <307583964@qq.com>
2019-11-04 18:11:09 +0800

    新增安卓用户紧急通知页面
    新增安卓用户紧急通知页面

---
+++ pages/scatter/notification.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="notification">
+    <div class="notification-div">
+      <img
+        class="notification-div-img"
+        src="https://qn-static.landi.com/uploadtoole6134e881eaa4760616ff1c931b4c0f5.png"
+      />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'notification',
+  head () {
+    return {
+      title: '紧急通知'
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+#notification {
+  background: #fff;
+  width: 100vw;
+  height: 100%;
+}
+.notification-div {
+    width: 100%;
+    height: 100%;
+    &-img {
+        width: 100%;
+        height: 100%;
+    }
+}
+</style>
