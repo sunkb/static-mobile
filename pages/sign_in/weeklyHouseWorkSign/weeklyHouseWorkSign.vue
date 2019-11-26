15e89203a9426a88cf8cd76b6038186be7d1fab3
jiafan <307583964@qq.com>
2019-11-26 18:18:02 +0800

    路由地址的修改
    路由地址的修改

--- pages/sign_in/weeklyHouseWorkSign/weeklyHouseWorkSign.vue
+++ pages/sign_in/weeklyHouseWorkSign/weeklyHouseWorkSign.vue
@@ -219,8 +219,8 @@ export default {
      */
     finSignBtn: function() {
       // http://192.168.29.119:3000/
-      // window.location = `${process.env.BASE_URL}/sign_in/upLoadVideo/upLoadVideo?homeworkId=${this.homeworkId}`;
-      window.location = `http://192.168.29.119:3000/sign_in/upLoadVideo/upLoadVideo?homeworkId=${this.homeworkId}`;
+      window.location = `${process.env.BASE_URL}/sign_in/upLoadVideo/upLoadVideo?homeworkId=${this.homeworkId}`;
+      // window.location = `http://192.168.29.119:3000/sign_in/upLoadVideo/upLoadVideo?homeworkId=${this.homeworkId}`;
     },
     /**
      * 点击历史打卡记录跳转到详情页面
@@ -232,7 +232,7 @@ export default {
       //   path: "/sign_in/signInInfom/signInInfom",
       //   query: { id: studentId ,homework_Id: itemObj.id }
       // });
-      window.location = `http://192.168.29.119:3000/sign_in/signInInfom/signInInfom?id=${studentId}&homework_Id=${itemObj.id}`; // 此路由需要设置
+      window.location = `${process.env.BASE_URL}/sign_in/signInInfom/signInInfom?id=${studentId}&homework_Id=${itemObj.id}`; // 此路由需要设置
     },
     // 下拉加载数据
     onLoad() {
