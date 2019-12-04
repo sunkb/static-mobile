88d79efa8a3ab2012cbb1e495831961d6742558b
jiafan <307583964@qq.com>
2019-12-04 15:21:31 +0800

    轮播效果
    轮播效果

--- pages/bruin/my_bruin/abstract.vue
+++ pages/bruin/my_bruin/abstract.vue
@@ -3,13 +3,38 @@
     <div class="abstract-background" @click="cancelShow"></div>
     <div class="abstract-stage">
       <div class="abstract-stage-button">
-        <div></div>
-        <div class="abstract-stage-button-content">放入熊库</div>
+        <div class="abstract-stage-button-swipe">
+          <van-swipe :autoplay="300">
+            <van-swipe-item>
+              <div style="width: 591px;height: 302px;display:flex;margin-left:96px;">
+                <img style="width: 139px;height: 150px;" src="../../../assets/bruin/img/test11/test.png"/>
+              </div>
+            </van-swipe-item>
+            <van-swipe-item>
+              <div style="width: 591px;height: 302px;display:flex;margin-left:96px;">
+                <img style="width: 139px;height: 150px;" src="../../../assets/bruin/img/test11/test.png"/>
+              </div>
+            </van-swipe-item>
+            <van-swipe-item>
+              <div style="width: 591px;height: 302px;display:flex;margin-left:96px;">
+                <img style="width: 139px;height: 150px;" src="../../../assets/bruin/img/test11/test.png"/>
+              </div>
+            </van-swipe-item>
+            <van-swipe-item>
+              <div style="width: 591px;height: 302px;display:flex;margin-left:96px;">
+                <img style="width: 139px;height: 150px;" src="../../../assets/bruin/img/test11/test.png"/>
+              </div>
+            </van-swipe-item>
+          </van-swipe>
+        </div>
+        <div class="abstract-stage-button-content" @click="ssss">放入熊库</div>
       </div>
     </div>
   </div>
 </template>
 <script>
+import { Swipe, SwipeItem } from 'vant';
+import "vant/lib/index.css";
 export default {
   name: 'abstract',
   props: {
@@ -18,12 +43,24 @@ export default {
       default: false
     }
   },
+  data() {
+    return {
+      isShowIndicators: false
+    }
+  },
+  components: {
+    'van-swipe': Swipe,
+    'van-swipe-item': SwipeItem
+  },
   created () {
     console.log(1111111111)
   },
   methods: {
     cancelShow () {
       this.$emit('fcancelShow')
+    },
+    ssss() {
+      console.log(111111111)
     }
   }
 }
@@ -53,7 +90,7 @@ export default {
     top: 131px;
     left: 50%;
     margin-left: -296px;
-    z-index: 10000;
+    z-index: 1000;
     &-button {
       width: 314px;
       height: 99px;
@@ -67,6 +104,13 @@ export default {
       display: flex;
       justify-content: center;
       align-items: center;
+      &-swipe {
+        width: 592px;
+        height: 300px;
+        position: absolute;
+        top: -430px;
+        z-index: 10000;
+      }
       &-content {
         font-size: 44px;
         font-family: PingFang SC;
