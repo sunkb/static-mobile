2af5dade5e2920b41b5e39c1853fc726d7084adb
tony <tony.tang@landi.com>
2019-12-04 18:17:21 +0800

    重新打包

--- api/danyue-diy-poster.js
+++ api/danyue-diy-poster.js
@@ -4,12 +4,12 @@ export default {
 
     // 检查个性化海报的基本信息
     async posterIndex(urlLink) {
-        const data = { fullUrl: urlLink };
-        const url = '/Mobile/DiyPoster/index';
+        const data = { url: urlLink };
+        const url = '/Mobile/Wx/share';
         console.log('data',data);
         console.log('url',url);
         const res = await axios.post(url, data);
-        //return res;
+        return res;
     },
     // 获取微信上传图片
     async getPosterImage(mediaId) {
