f012d3bfeaa56b98afe43487c39630002a9c01a2
jiafan <307583964@qq.com>
2019-12-05 11:02:33 +0800

    集五熊规则页面
    集五熊规则页面

--- pages/bruin/rule/index.vue
+++ pages/bruin/rule/index.vue
@@ -4,6 +4,10 @@
       class="bruin-rule-img"
       src="https://qn-static.landi.com/uploadtool268ec3eea4bf92331eeea6314e4b4df3.png"
     />
+    <div class="home-header">
+      <img class="home-header-back" src="../../../assets/bruin/img/back_home_button.png" />
+      <img class="home-header-work" src="../../../assets/bruin/img/home_bruin.png" />
+    </div>
   </div>
 </template>
 <script>
@@ -18,6 +22,25 @@ export default {
   width: 100vw;
   &-img {
     width: 100vw;
+  }
+  .home-header {
+    padding-top: 77px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    position: absolute;
+    top: 0;
+    z-index: 1000;
+    &-back {
+      width: 112px;
+      height: 71px;
+    }
+    &-work {
+      width: 96px;
+      height: 94px;
+      margin-right: 18px;
+    }
   }
 }
 </style>
