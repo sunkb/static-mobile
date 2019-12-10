2f11f588b8eca24d8c1ffa2e7d8cc781b0e5841f
jiafan <307583964@qq.com>
2019-12-10 10:27:21 +0800

    测试
    测试

--- pages/bruin/index.vue
+++ pages/bruin/index.vue
@@ -96,6 +96,7 @@ export default {
     // 用于微信分享的数据的接口请求
     async wxShare () {
       const curUrl = encodeURIComponent(window.location.host)
+      console.log(window.location.href)
       const activityID = 1
       try {
         const res = await axios.get(`${API.WX_SHARE}?activity_id=${activityID}&url=${curUrl}`)
