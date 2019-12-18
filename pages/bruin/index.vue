f745062ff58b7876ff114a755191535a8fae04da
jiafan <307583964@qq.com>
2019-12-18 11:32:20 +0800

    集五熊二期需求
    集五熊二期需求

--- pages/bruin/index.vue
+++ pages/bruin/index.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="home">
     <div class="home-background">
-      <div class="home-marquee">
-        {{pmdInfo}}
-      </div>
+      <div class="home-marquee">{{pmdInfo}}</div>
       <div class="home-header">
         <img class="home-header-ways" src="../../assets/bruin/img/ways.png" @click="goToRule" />
         <div class="home-header-right">
@@ -12,30 +10,71 @@
             src="../../assets/bruin/img/home_bruin.png"
             @click="goToMyBruin"
           />
-          <div v-show="curAwardBruinNum == 0 ? false : true" class="home-header-right-times"><div>{{curAwardBruinNum}}</div></div>
+          <div v-show="curAwardBruinNum == 0 ? false : true" class="home-header-right-times">
+            <div>{{curAwardBruinNum}}</div>
+          </div>
         </div>
       </div>
       <div class="home-pitch">
-        <div class="home-pitch-first">
-          <img class="hot" src="../../assets/bruin/img/hot.png" />
-          <img class="home-pitch-first-img" src="../../assets/bruin/img/pitch_button.png" />
+        <div class="home-pitch-first" @click="cutButton('first')">
+          <img v-show="chooseButton" class="hot" src="../../assets/bruin/img/hot.png" />
+          <img
+            v-show="chooseButton"
+            class="home-pitch-first-img"
+            src="../../assets/bruin/img/first_button_click.png"
+          />
+          <img
+            v-show="!chooseButton"
+            class="home-pitch-first-img"
+            src="../../assets/bruin/img/first_button.png"
+          />
         </div>
-        <div class="home-pitch-second">
-          <img class="home-pitch-second-img" src="../../assets/bruin/img/second_button_disable.png" />
+        <div class="home-pitch-second" @click="cutButton('second')">
+          <img
+            v-show="chooseButton"
+            class="home-pitch-second-img"
+            src="../../assets/bruin/img/second_button.png"
+          />
+          <img v-show="!chooseButton" class="hot" src="../../assets/bruin/img/hot.png" />
+          <img
+            v-show="!chooseButton"
+            class="home-pitch-second-img"
+            src="../../assets/bruin/img/second_button_click.png"
+          />
         </div>
       </div>
-      <div class="home-entrance">
+      <div class="home-entrance" v-show="chooseButton">
         <div v-show="firstChoose" class="home-entrance-new" @click="goToMyBruin">
           <img class="home-entrance-new-img" src="../../assets/bruin/img/first_use.png" />
         </div>
         <div class="home-entrance-invite" @click="inviteAction">
           <img class="home-entrance-invite-img" src="../../assets/bruin/img/invite_button.png" />
-          <div class="home-entrance-invite-num"><div>已邀请</div><div class="home-entrance-invite-num-color">{{activityData.invite_num}}</div><div>人</div></div>
+          <div class="home-entrance-invite-num">
+            <div>已邀请</div>
+            <div class="home-entrance-invite-num-color">{{activityData.invite_num}}</div>
+            <div>人</div>
+          </div>
         </div>
         <div class="home-entrance-renew">
           <img class="home-entrance-renew-img" src="../../assets/bruin/img/buy_course.png" />
-          <img v-show="activityData.is_buy ? true : false" class="home-entrance-renew-finsh-img" src="../../assets/bruin/img/finsh.png"/>
-          <img v-show="activityData.is_buy ? false : true" class="home-entrance-renew-unfinsh-img" src="../../assets/bruin/img/unfinsh.png"/>
+          <img
+            v-show="activityData.is_buy ? true : false"
+            class="home-entrance-renew-finsh-img"
+            src="../../assets/bruin/img/finsh.png"
+          />
+          <img
+            v-show="activityData.is_buy ? false : true"
+            class="home-entrance-renew-unfinsh-img"
+            src="../../assets/bruin/img/unfinsh.png"
+          />
+        </div>
+      </div>
+      <div class="home-entrance2" v-show="!chooseButton">
+        <div class="home-entrance2-coin">
+          <img class="home-entrance2-coin-img" src="https://qn-static.landi.com/uploadtool5e03034915263870097a2e4a2d42d2c8.png" />
+        </div>
+        <div class="home-entrance2-gua">
+          <img class="home-entrance2-gua-img" src="https://qn-static.landi.com/uploadtool7e2338631adc37e4217a4053e1404dc2.png" />
         </div>
       </div>
       <div class="sharehelp" v-show="showShareHelp" @click="() => { showShareHelp = false }">
@@ -50,11 +89,11 @@ import { API } from '~/pages/bruin/consts'
 import axios from '~/utils/axios'
 import Toast from '~/components/Toast'
 import { initWX } from '~/pages/presentation/wx'
-import {Login} from '~/utils/core/logins'
+import { Login } from '~/utils/core/logins'
 import { PosterModal } from '~/components/presentation'
 export default {
   name: "home",
-  head() {
+  head () {
     return {
       title: "集五熊，赢千万课时"
     }
@@ -74,7 +113,8 @@ export default {
       },
       curAwardBruinNum: 0,
       isBuy: false,
-      firstChoose: false
+      firstChoose: false,
+      chooseButton: true  // 用于首页的第一波熊和第二波熊的按钮切换
     }
   },
   components: {
@@ -128,7 +168,7 @@ export default {
           wx.updateTimelineShareData(shareObj)
           wx.onMenuShareAppMessage(shareObj);
           wx.onMenuShareTimeline(shareObj);
-          wx.error(function(res){
+          wx.error(function (res) {
             console.log(res);
           });
         })
@@ -143,31 +183,31 @@ export default {
       try {
         const activityID = 1
         const res = await axios.get(`${API.BRUIN_PMD}?activity_id=${activityID}`)
-        if(!res.status) {
+        if (!res.status) {
           console.log(res.info)
-          return 
+          return
         }
         this.pmdInfo = res.data || '0人已集齐，2019年1月1日18：00开奖'
       } catch (err) {
-          console.log(res)
-          return
+        console.log(res)
+        return
       }
     },
     //登录或者注册模式选择
-    async gotoLoginRegister() {
+    async gotoLoginRegister () {
       let redirect_url = window.location.href;
-      redirect_url = removeParam('code',redirect_url);
-      redirect_url = removeParam('state',redirect_url);
+      redirect_url = removeParam('code', redirect_url);
+      redirect_url = removeParam('state', redirect_url);
       redirect_url = encodeURIComponent(redirect_url);
-      const loginUrl = process.env.ENV_API+'/mobile/login/index/#/login?redirect_url='+redirect_url;
+      const loginUrl = process.env.ENV_API + '/mobile/login/index/#/login?redirect_url=' + redirect_url;
       window.location = loginUrl;
     },
     // 活动详情的接口数据
-    async getActivityDetail() {
+    async getActivityDetail () {
       try {
         const activityId = 1
         const res = await axios.get(`${API.ACTIVITY_DETAIL}?activity_id=${activityId}`)
-        if(!res.status) {
+        if (!res.status) {
           console.log(res.info)
           return
         }
@@ -197,7 +237,7 @@ export default {
       const activityID = 1
       try {
         const res = await axios.get(`${API.MY_BRUIN}?activity_id=${activityID}`)
-        if(!res.status) {
+        if (!res.status) {
           console.log(res.info)
           return
         }
@@ -206,6 +246,14 @@ export default {
         console.log(err)
         return
       }
+    },
+    // 切换第几波熊的按钮
+    cutButton (type) {
+      if (type === 'first') {
+        this.chooseButton = true
+        return
+      }
+      this.chooseButton = false
     }
   },
   created () {
@@ -216,9 +264,9 @@ export default {
     try {
       const activityID = 1
       const res = await axios.get(`${API.BRUIN_PMD}?activity_id=${activityID}`)
-      if(!res.status) {
+      if (!res.status) {
         console.log(res.info)
-        return 
+        return
       }
       this.pmdInfo = res.data || '0人已集齐，2019年1月1日18：00开奖'
       this.wxShare()
@@ -227,27 +275,27 @@ export default {
       this.$refs['toast'].hideLoadingToast()
     } catch (err) {
       console.log(err)
-      if(err.response.status === 401){ // 用于判断是否登录过
+      if (err.response.status === 401) { // 用于判断是否登录过
         this.gotoLoginRegister()
         console.log(err.response.info)
       }
     }
   }
 }
-function removeParam(key, sourceURL) {
+function removeParam (key, sourceURL) {
   var rtn = sourceURL.split("?")[0],
-      param,
-      params_arr = [],
-      queryString = (sourceURL.indexOf("?") !== -1) ? sourceURL.split("?")[1] : "";
+    param,
+    params_arr = [],
+    queryString = (sourceURL.indexOf("?") !== -1) ? sourceURL.split("?")[1] : "";
   if (queryString !== "") {
-      params_arr = queryString.split("&");
-      for (var i = params_arr.length - 1; i >= 0; i -= 1) {
-          param = params_arr[i].split("=")[0];
-          if (param === key) {
-              params_arr.splice(i, 1);
-          }
+    params_arr = queryString.split("&");
+    for (var i = params_arr.length - 1; i >= 0; i -= 1) {
+      param = params_arr[i].split("=")[0];
+      if (param === key) {
+        params_arr.splice(i, 1);
       }
-      rtn = rtn + "?" + params_arr.join("&");
+    }
+    rtn = rtn + "?" + params_arr.join("&");
   }
   return rtn;
 }
@@ -266,11 +314,11 @@ function removeParam(key, sourceURL) {
     .home-marquee {
       height: 50px;
       width: 100%;
-      background:rgba(0,0,0,1);
-      opacity:0.5;
-      font-size:28px;
-      font-weight:400;
-      color:rgba(255,255,255,1);
+      background: rgba(0, 0, 0, 1);
+      opacity: 0.5;
+      font-size: 28px;
+      font-weight: 400;
+      color: rgba(255, 255, 255, 1);
       text-align: center;
       padding-top: 5px;
     }
@@ -292,12 +340,12 @@ function removeParam(key, sourceURL) {
           margin-right: 18px;
         }
         &-times {
-          width:30px;
-          height:30px;
-          background:rgba(255,54,0,1);
-          border-radius:50%;
-          font-weight:600;
-          color:rgba(255,255,255,1);
+          width: 30px;
+          height: 30px;
+          background: rgba(255, 54, 0, 1);
+          border-radius: 50%;
+          font-weight: 600;
+          color: rgba(255, 255, 255, 1);
           display: flex;
           align-items: center;
           justify-content: center;
@@ -313,8 +361,8 @@ function removeParam(key, sourceURL) {
       justify-content: center;
       position: relative;
       &-first {
-        width: 343px;
-        height: 100px;
+        width: 362px;
+        height: 104px;
         .hot {
           height: 61px;
           width: 42px;
@@ -323,16 +371,24 @@ function removeParam(key, sourceURL) {
           left: 60px;
         }
         &-img {
-          width: 343px;
-          height: 100px;
+          width: 362px;
+          height: 104px;
         }
       }
       &-second {
-        width: 343px;
-        height: 100px;
+        width: 362px;
+        height: 104px;
+        position: relative;
+        .hot {
+          height: 61px;
+          width: 42px;
+          position: absolute;
+          top: -20px;
+          left: 40px;
+        }
         &-img {
-          width: 343px;
-          height: 100px;
+          width: 362px;
+          height: 104px;
         }
       }
     }
@@ -360,8 +416,8 @@ function removeParam(key, sourceURL) {
         &-num {
           display: flex;
           align-items: center;
-          font-size:20px;
-          color:rgba(117,117,117,1);
+          font-size: 20px;
+          color: rgba(117, 117, 117, 1);
           position: absolute;
           bottom: 30px;
           left: 310px;
@@ -391,6 +447,29 @@ function removeParam(key, sourceURL) {
           position: absolute;
           right: 38px;
           top: 36px;
+        }
+      }
+    }
+    .home-entrance2 {
+      margin-top: 45px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      &-coin {
+        width: 630px;
+        height: 241px;
+        &-img {
+          width: 630px;
+          height: 241px;
+        }
+      }
+      &-gua {
+        width: 630px;
+        height: 241px;
+        position: relative;
+        &-img {
+          width: 630px;
+          height: 241px;
         }
       }
     }
